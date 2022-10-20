6aab53a98b4481d364daa6bdea74d06849b419b2
Léo Clénet <113632638+leoclenet@users.noreply.github.com>
2022-10-20 11:41:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,52 +3,45 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import time as tm
-from look_estimator import LookEstimator
+
+from person_position import PersonPosition
+from person_looking import PersonLooking
 
 
 
 def click_event(event, x, y, flags, params):
-    # checking for left mouse clicks
     if event == cv2.EVENT_LBUTTONDOWN:
-        look_estimator.change_position(x, y)
-    if event == cv2.EVENT_MBUTTONDOWN:
-        print("middle")
-
- 
-
-'''
-1. Choose between importing a video or using a webcam in live
-2. Execute main.py with your terminal
-'''
+        person_looking.change_position(x, y)
 
 
+
+# Video parameters
 cap = cv2.VideoCapture('/home/kali/Documents/Stage/Sources/video4.mp4') # Video
 # cap = cv2.VideoCapture(0) # Camera
+frameRate = 5   # Number of images per second
 
-# Number of images per second
-frameRate = 5
 
-# Parameters
+# Persons & precision parameters
 names = ['0', '1', '2', '3', '4', '5']
 angleRange = 15
+framePrecision = 130    # Frame size precision
 
-
-# Frame size precision
-framePrecision = 130
 
 # Detection & tracking
 mp_face_mesh = mp.solutions.face_mesh
-face_mesh1 = [mp_face_mesh.FaceMesh(min_detection_confidence=0.6,
-                                    min_tracking_confidence=0.6) for x in names]
+face_mesh1 = [mp_face_mesh.FaceMesh(min_detection_confidence=0.5,
+                                    min_tracking_confidence=0.5) for x in names]
 
-face_mesh2 = [mp_face_mesh.FaceMesh(min_detection_confidence=0.6,
-                                    min_tracking_confidence=0.6) for x in names]
+face_mesh2 = [mp_face_mesh.FaceMesh(min_detection_confidence=0.5,
+                                    min_tracking_confidence=0.5) for x in names]
+
 
 # Initialize
-look_estimator = LookEstimator(names, frameRate)
+person_position = PersonPosition()
+person_looking = PersonLooking(names, frameRate)
 
-# get the colors
-colorCircle = look_estimator.get_colors()
+# Get the colors
+colorCircle = person_looking.get_colors()
 
 
 
@@ -56,106 +49,16 @@ while cap.isOpened():
 
     success, frame = cap.read()
 
-    angles = []
+    # Determine where are the persons in the frame
+    angles = person_position.facial_recognition(frame, angleRange, colorCircle, face_mesh1)
+    nbPerson = len(angles)
 
-    # Draw graduation angle on video
-    look_estimator.graduation_angle_video(frame)
+    # Calculate where are the persons
+    personsFrame = person_looking.persons_position_in_frame(angles, nbPerson)
 
-    pNb = -1
-
-    for i in range(0, 360, angleRange):
-
-        if(i < 180):
-            x1 = int(i*1920/180)
-            x2 = int((i+angleRange)*1920/180)
-            y1 = 0
-            y2 = 540
-            cutf = frame[y1:y2, x1:x2]
-        else:
-            x1 = int(i*1920/360)
-            x2 = int((i+angleRange)*1920/360)
-            y1 = 540
-            y2 = 1080
-            cutf = frame[y1:y2, x1:x2]
-
-        cutf = cv2.cvtColor(cutf, cv2.COLOR_BGR2RGB)
-        results = face_mesh1[pNb].process(cutf)
-
-        if results.multi_face_landmarks:
-
-            pNb += 1
-            
-            # Convert the color space from RGB to BGR to display well with Opencv
-            cutf = cv2.cvtColor(cutf, cv2.COLOR_RGB2BGR)
-
-            face_coordination_in_real_world = np.array([
-                [285, 528, 200],
-                [285, 371, 152],
-                [197, 574, 128],
-                [173, 425, 108],
-                [360, 574, 128],
-                [391, 425, 108]
-            ], dtype=np.float64)
-
-            h, w, _ = cutf.shape
-            face_coordination_in_image = []
-
-            if results.multi_face_landmarks:
-
-                for face_landmarks in results.multi_face_landmarks:
-
-
-                    for idx, lm in enumerate(face_landmarks.landmark):
-                        if idx in [1, 9, 57, 130, 287, 359]:
-                            x, y = int(lm.x * w), int(lm.y * h)
-                            face_coordination_in_image.append([x, y])
-
-                    face_coordination_in_image = np.array(face_coordination_in_image,
-                                                        dtype=np.float64)
-                    
-                    # Calculate the coord of the persons
-                    cordX = int(x1 + face_coordination_in_image[0][0])
-                    
-                    if(i < 180):
-                        cordY = int(face_coordination_in_image[0][1])
-                    else:
-                        cordY = int(540 + face_coordination_in_image[0][1])
-                    
-                    cv2.circle(frame, (cordX, cordY), 4, colorCircle[pNb], -1)
-                    cv2.rectangle(frame, (cordX-40, cordY-40), (cordX+40, cordY+40), colorCircle[pNb], 2)
-
-                    if(i < 180):
-                        cv2.circle(frame, (cordX, 15), 6, (0, 0, 255), -1)
-                    else:
-                        cv2.circle(frame, (cordX, 550), 6, (0, 0, 255), -1)
-
-                    # Calculate the angle
-                    if(i < 180):
-                        angles.append(int(cordX*180/1920))
-                    else:
-                        angles.append(int(180 + cordX*180/1920))
-
-
-    # Number of persons for each frame
-    numberPerson = len(angles)
-
-    # Calculate where are the persons in the frame
-    personsFrame = look_estimator.persons_position_in_frame(angles, numberPerson)
-
-    # Write the names of persons in frame
-    look_estimator.draw_persons_in_frame(frame, names, framePrecision, numberPerson)
-
-    # Draw cut
-    look_estimator.draw_image_cut(frame, angles, framePrecision, numberPerson)
-
-    # Calculate where are the persons in the circle
-    look_estimator.persons_position_in_circle(angles, numberPerson)
-
-    # Draw the situation
-    look_estimator.draw_situation(frame, names, numberPerson)
-
-    # Draw graduation angle on schema
-    look_estimator.graduation_angle_schema(frame)
+    # Draw the situation on video & schema
+    person_looking.draw_on_video(frame, names, framePrecision, nbPerson, angles)
+    person_looking.draw_schema(frame, names, nbPerson, angles)
 
 
     for i in range(0, len(angles)):
@@ -213,7 +116,7 @@ while cap.isOpened():
                 # Use Rodrigues function to convert rotation vector to matrix
                 rotation_matrix, jacobian = cv2.Rodrigues(rotation_vec)
 
-                result = look_estimator.rotation_matrix_to_angles(rotation_matrix)
+                result = person_looking.rotation_matrix_to_angles(rotation_matrix)
 
                 # Calculate & show the results of pitch, yaw & roll
                 for l, info in enumerate(zip(('pitch', 'yaw'), result)):
@@ -225,14 +128,11 @@ while cap.isOpened():
                     if(k == 'yaw'): yaw = int(v)
                     if(k == 'pitch'): pitch = int(v)
                     
-                    # Show results
-                    look_estimator.draw_yaw(frame, text, i, l, framePrecision)
-
-                # Draw looking lines
-                look_estimator.draw_lines(frame, yaw, angles, names, i)
+                # Draw results
+                person_looking.draw_result(frame, yaw, pitch, i, framePrecision, angles, names)
 
                 # Looking position
-                look_estimator.looking_position(frame, angles, yaw, names, framePrecision, i, numberPerson)
+                person_looking.looking_position(frame, angles, yaw, names, framePrecision, i, nbPerson)
 
               
         
@@ -240,11 +140,8 @@ while cap.isOpened():
 
     cv2.setMouseCallback('Head Pose Angles', click_event)
 
-    #tm.sleep(0.25)
-
     # Quit if press Escape
     if cv2.waitKey(5) & 0xFF == 27:
         break
 
 cap.release()
-
